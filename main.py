9bf7d5f02102aa0c699a4a0d9ac1e9f043115ebb
koobawuah <me@bawuahboakye.com>
2022-04-30 23:23:04 +0000

    Some changes in code with different python libraries to help scraping website easy

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+import time
 from bs4 import BeautifulSoup
-import requests 
+from selenium import webdriver
+
+from requests_html import HTMLSession
 
 base_url = 'https://www.premierleague.com/fixtures'
 spec_url = 'https://www.premierleague.com/fixtures?co=1&cl='
+url = 'https://servinn-8acdd.web.app'
 
-epl_fix_page = requests.get(base_url)
-content = BeautifulSoup(epl_fix_page, 'lxml')
-fixtures = content.find_all('ul', class_='matchList')
+#driver = webdriver.Chrome('/Applications/chromedriver')
+#servinnweb = driver.get(url)
+#time.sleep(5)
 
-print(content)
+#page = driver.find_element(By.TAG_NAME, 'div')
+
+#driver.quit()
+
+
+s = HTMLSession()
+ss = s.get(base_url)
+
+ss.html.render(keep_page=True)
+
+
+print(ss.html.page)
