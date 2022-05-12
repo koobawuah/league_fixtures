5aa394195024a808d8278b5e7770ed459f177b65
koobawuah <me@bawuahboakye.com>
2022-05-12 09:42:00 +0000

    Different set of tests to an actual API endpoint

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-import time
-from bs4 import BeautifulSoup
-from selenium import webdriver
+import requests
 
-from requests_html import HTMLSession
+url = 'https://footballapi.pulselive.com/football/fixtures?comps=1&teams=2&compSeasons=418&page=0&pageSize=40&sort=asc&statuses=U,L&altIds=true'
 
-base_url = 'https://www.premierleague.com/fixtures'
-spec_url = 'https://www.premierleague.com/fixtures?co=1&cl='
-url = 'https://servinn-8acdd.web.app'
+aurl = 'https://footballapi.pulselive.com/football/fixtures?comps=1&teams=1,2,130,131,43,4,6,7,9,26,10,11,12,23,14,20,21,33,25,38&compSeasons=418&page=0&pageSize=40&sort=asc&statuses=U,L&altIds=true'
 
-#driver = webdriver.Chrome('/Applications/chromedriver')
-#servinnweb = driver.get(url)
-#time.sleep(5)
+headers = { 
+    "Authority": "footballapi.pulselive.com",
+    "Method": "GET",
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Origin": "https://www.premierleague.com",
+    "Referer": "https://www.premierleague.com/",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"
+}
 
-#page = driver.find_element(By.TAG_NAME, 'div')
-
-#driver.quit()
-
-
-s = HTMLSession()
-ss = s.get(base_url)
-
-ss.html.render(keep_page=True)
+epl_fix = requests.get(url, headers=headers)
+epl = requests.get(aurl, headers=headers)
 
 
-print(ss.html.page)
+print(epl.url)
+
